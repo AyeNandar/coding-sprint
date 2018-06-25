36d3ccf76725637431522584a668760eee610a75
AyeNandar <andhandh1381@gmail.com>
2018-06-24 21:40:48 -0700

    to print your address,your computer

---
+++ python-exercises/ex15.py
@@ -0,0 +1,13 @@
+from sys import argv
+script,user_name = argv
+prompt='> '
+print("Hi ", user_name, "I m the", script," script.")
+print("Do you like me ", user_name,"?")
+likes = input(prompt)
+print("Where do you live ", user_name," ?")
+lives = input(prompt)
+print("What kind of computer do you have ?")
+computer = input(prompt)
+print("Alright,so you said ", likes," about liking me.You live in", lives," Not sure that is " , computer," Nice.")
+
+
